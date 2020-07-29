e19708b00f28ef0f4c07a128db883729eb21b86b
Morteza Esmailpour <Morteza88@live.com>
2020-07-29 19:33:56 +0430

    add ICurrencyConverter interface from code challenge

--- CurrencyConverterLibrary/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace CurrencyConverterLibrary
-{
-    public class Class1
-    {
-    }
-}
---
+++ CurrencyConverterLibrary/CurrencyConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CurrencyConverterLibrary
+{
+    public class CurrencyConverter : ICurrencyConverter
+    {
+        public void ClearConfiguration()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Convert(string fromCurrency, string toCurrency, double amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateConfiguration(IEnumerable<Tuple<string, string, double>> conversionRates)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
