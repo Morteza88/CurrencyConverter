f8031629dc9bb52606be6baf506610f1f57f962d
Morteza Esmailpour <Morteza88@live.com>
2020-07-29 21:12:34 +0430

    add unit test for ShortestPathFunction

--- CurrencyConverterlibrary.NUnitTests/FindShortestPathUnitTest.cs
+++ CurrencyConverterlibrary.NUnitTests/FindShortestPathUnitTest.cs
@@ -1,5 +1,7 @@
 using CurrencyConverterLibrary;
+using CurrencyConverterLibrary.Utils;
 using NUnit.Framework;
+using NUnit.Framework.Constraints;
 using System;
 using System.Collections.Generic;
 
@@ -24,7 +26,33 @@ namespace CurrencyConverterlibrary.NUnitTests
             currencyConverter.UpdateConfiguration(rates);
 
             double result = currencyConverter.Convert("CAD", "EUR", 1);
-            Assert.AreEqual(0.86/1.34,result);
+            Assert.AreEqual(0.86 / 1.34, result);
+        }
+        [Test]
+        public void GetShortestPathFunction_NormalGraph_ReturnShortestPath()
+        {
+            List<Tuple<string, string>> edges = new List<Tuple<string, string>>();
+            edges.Add(Tuple.Create("a", "b"));
+            edges.Add(Tuple.Create("a", "d"));
+            edges.Add(Tuple.Create("b", "c"));
+            edges.Add(Tuple.Create("d", "e"));
+            edges.Add(Tuple.Create("d", "h"));
+            edges.Add(Tuple.Create("e", "f"));
+            edges.Add(Tuple.Create("e", "g"));
+            edges.Add(Tuple.Create("e", "h"));
+            edges.Add(Tuple.Create("f", "g"));
+            edges.Add(Tuple.Create("g", "h"));
+            Graph<string> graph = Graph<string>.CreateGraph(edges);
+            var shortestPathFunc = graph.GetShortestPathFunction("a");
+            
+            Assert.AreEqual(new List<string> {"a" }, shortestPathFunc("a"));
+            Assert.AreEqual(new List<string> {"a","b" }, shortestPathFunc("b"));
+            Assert.AreEqual(new List<string> {"a","b","c" }, shortestPathFunc("c"));
+            Assert.AreEqual(new List<string> {"a","d" }, shortestPathFunc("d"));
+            Assert.AreEqual(new List<string> {"a","d","e" }, shortestPathFunc("e")); 
+            Assert.AreEqual(new List<string> { "a", "d", "e", "f" }, shortestPathFunc("f"));
+            Assert.That(shortestPathFunc("g"), Is.EqualTo(new List<string> { "a", "d", "e", "g" }) | Is.EqualTo(new List<string> { "a", "d", "h", "g" }));
+            Assert.AreEqual(new List<string> {"a","d","h" }, shortestPathFunc("h"));
         }
     }
 }
