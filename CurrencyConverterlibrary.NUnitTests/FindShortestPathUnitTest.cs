4cb333d01c34e3f4108538ab6faffe0a05d63268
Morteza Esmailpour <Morteza88@live.com>
2020-07-29 21:47:10 +0430

    add Unconnected Graph unit test

--- CurrencyConverterlibrary.NUnitTests/FindShortestPathUnitTest.cs
+++ CurrencyConverterlibrary.NUnitTests/FindShortestPathUnitTest.cs
@@ -28,9 +28,12 @@ namespace CurrencyConverterlibrary.NUnitTests
             double result = currencyConverter.Convert("CAD", "EUR", 1);
             Assert.AreEqual(0.86 / 1.34, result);
         }
+
+
         [Test]
-        public void GetShortestPathFunction_NormalGraph_ReturnShortestPath()
+        public void GetShortestPathFunction_ConnectedGraph_ReturnShortestPath()
         {
+            //Arrenge
             List<Tuple<string, string>> edges = new List<Tuple<string, string>>();
             edges.Add(Tuple.Create("a", "b"));
             edges.Add(Tuple.Create("a", "d"));
@@ -43,16 +46,50 @@ namespace CurrencyConverterlibrary.NUnitTests
             edges.Add(Tuple.Create("f", "g"));
             edges.Add(Tuple.Create("g", "h"));
             Graph<string> graph = Graph<string>.CreateGraph(edges);
+
+            //Action
             var shortestPathFunc = graph.GetShortestPathFunction("a");
-            
-            Assert.AreEqual(new List<string> {"a" }, shortestPathFunc("a"));
-            Assert.AreEqual(new List<string> {"a","b" }, shortestPathFunc("b"));
-            Assert.AreEqual(new List<string> {"a","b","c" }, shortestPathFunc("c"));
-            Assert.AreEqual(new List<string> {"a","d" }, shortestPathFunc("d"));
-            Assert.AreEqual(new List<string> {"a","d","e" }, shortestPathFunc("e")); 
+
+            //Assert
+            Assert.AreEqual(new List<string> { "a" }, shortestPathFunc("a"));
+            Assert.AreEqual(new List<string> { "a", "b" }, shortestPathFunc("b"));
+            Assert.AreEqual(new List<string> { "a", "b", "c" }, shortestPathFunc("c"));
+            Assert.AreEqual(new List<string> { "a", "d" }, shortestPathFunc("d"));
+            Assert.AreEqual(new List<string> { "a", "d", "e" }, shortestPathFunc("e"));
             Assert.AreEqual(new List<string> { "a", "d", "e", "f" }, shortestPathFunc("f"));
-            Assert.That(shortestPathFunc("g"), Is.EqualTo(new List<string> { "a", "d", "e", "g" }) | Is.EqualTo(new List<string> { "a", "d", "h", "g" }));
-            Assert.AreEqual(new List<string> {"a","d","h" }, shortestPathFunc("h"));
+            Assert.That(shortestPathFunc("g"), Is.EqualTo(new List<string> { "a", "d", "e", "g" }) | Is.EqualTo(new List<string> { "a", "r", "h", "g" }));
+            Assert.AreEqual(new List<string> { "a", "d", "h" }, shortestPathFunc("h"));
+        }
+
+
+        [Test]
+        public void GetShortestPathFunction_UnconnectedGraph_ReturnShortestPath()
+        {
+            //Arrenge
+            List<Tuple<string, string>> edges = new List<Tuple<string, string>>();
+            edges.Add(Tuple.Create("a", "b"));
+            edges.Add(Tuple.Create("a", "d"));
+            edges.Add(Tuple.Create("b", "c"));
+            edges.Add(Tuple.Create("c", "d"));
+            edges.Add(Tuple.Create("e", "f"));
+            edges.Add(Tuple.Create("e", "g"));
+            edges.Add(Tuple.Create("e", "h"));
+            edges.Add(Tuple.Create("f", "g"));
+            edges.Add(Tuple.Create("g", "h"));
+            Graph<string> graph = Graph<string>.CreateGraph(edges);
+
+            //Action
+            var shortestPathFunc = graph.GetShortestPathFunction("a");
+
+            //Assert
+            Assert.AreEqual(new List<string> { "a" }, shortestPathFunc("a"));
+            Assert.AreEqual(new List<string> { "a", "b" }, shortestPathFunc("b"));
+            Assert.AreEqual(new List<string> { "a", "b", "c" }, shortestPathFunc("c"));
+            Assert.AreEqual(new List<string> { "a", "d" }, shortestPathFunc("d"));
+            Assert.AreEqual(new List<string> { }, shortestPathFunc("e"));
+            Assert.AreEqual(new List<string> { }, shortestPathFunc("f"));
+            Assert.AreEqual(new List<string> { }, shortestPathFunc("h"));
+            Assert.AreEqual(new List<string> { }, shortestPathFunc("h"));
         }
     }
 }
